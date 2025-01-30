5a2efbf52bcb34548c355748c4bda93f0cc9134e
samuel-wambui <147292274+samuel-wambui@users.noreply.github.com>
2025-01-30 22:22:37 +0300

    Move requirements.txt to the root directory

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -201,3 +202,5 @@ LOGGING = {
     },
 }
 AUTH_USER_MODEL = 'Authorization.User'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
